7b87efda3fa3f9879980596a3c4ece617b687281
Matuk <mateusmatuk@fai-mg.br>
2020-03-02 20:50:49 -0300

    primeiro commit

---
+++ src/fai/br/projeto03/Main.java
@@ -0,0 +1,19 @@
+package fai.br.projeto03;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	Main app = new Main();
+	app.start();
+
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ex3");
+		
+	}
+
+}
